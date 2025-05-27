bda89a325a45961c5d77c92133709bba161b0b9d
juanmariiaa <arizaserranojuanmaria@gmail.com>
2025-05-27 21:00:29 +0200

    ENVINROMENT

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://localhost:8080/api',
-    googleClientId: '492097747607-1l3j2unsnkp8eq6ugib1bda9truoan1g.apps.googleusercontent.com'
-  };
+  production: true,
+  apiUrl: 'http://ec2-15-188-47-114.eu-west-3.compute.amazonaws.com:8080/api',
+  googleClientId:
+    '492097747607-1l3j2unsnkp8eq6ugib1bda9truoan1g.apps.googleusercontent.com',
+};
