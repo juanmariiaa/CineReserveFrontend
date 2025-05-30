ec12b7712957d451fa834a68d546c7d78bd5a165
juanmariiaa <arizaserranojuanmaria@gmail.com>
2025-05-30 09:59:40 +0200

    Update environment configurations for development and production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://ec2-13-37-42-80.eu-west-3.compute.amazonaws.com/api',
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
   googleClientId:
     '492097747607-1l3j2unsnkp8eq6ugib1bda9truoan1g.apps.googleusercontent.com',
-  allowedDomains: ['localhost:4200', 'ec2-52-47-83-176.eu-west-3.compute.amazonaws.com'],
+  allowedDomains: ['localhost:4200', 'localhost:8080', 'ec2-13-37-42-80.eu-west-3.compute.amazonaws.com'],
 };
